7d4898a60e7ff09e4c81e49c7629481144d499bb
Magnus <magnus@hvass-labs.org>
2019-09-30 12:13:23 +0100

    setup.py added long description to be shown on PyPi.org

--- setup.py
+++ setup.py
@@ -9,8 +9,14 @@ setup(
     name='simfin',
     packages=['simfin'],
     version=MY_VERSION,
-    description='Simple financial data from www.simfin.com',
-    author=['Hvass-Labs', 'SimFin'],
+    description='Simple financial data for Python',
+    long_description='SimFin makes it easy to obtain and use financial'
+                     'data in Python. It automatically downloads data'
+                     'from the [SimFin](https://www.simfin.com/) server,'
+                     'saves the data to disk for future use, and loads'
+                     'the data into Pandas DataFrames.',
+    long_description_content_type="text/markdown",
+    author='SimFin',
     author_email='info@simfin.com',
     url='https://github.com/simfin/simfin',
     license='MIT',

2a7e6505af714a821b6865149d2aa521d9f37e8f
Magnus <magnus@hvass-labs.org>
2019-10-23 12:11:23 +0100

    Generated names.py

---
+++ simfin/names_extra.py
@@ -0,0 +1,37 @@
+##########################################################################
+#
+# Names that makes it easier to address individual data-columns.
+#
+# This supplements names.py which has been auto-generated from the SimFin
+# database. This allows us to define more convenience names than are
+# available in the SimFin database. This module is imported in names.py
+# so all the names defined here will also be available from names.py
+#
+##########################################################################
+# SimFin - Simple financial data for Python.
+# www.simfin.com - www.github.com/simfin/simfin
+# See README.md for instructions and LICENSE.txt for license details.
+##########################################################################
+
+# Sales / Number of Shares.
+SALES_PER_SHARE = 'Sales Per Share'
+
+# Net Income / Number of Shares.
+EARNINGS_PER_SHARE = 'Earnings Per Share'
+
+# Price / Earnings Ratio.
+PE = 'P/E'
+
+# Price / Sales Ratio.
+PSALES = 'P/Sales'
+
+# Net Income / Revenue.
+NET_PROFIT_MARGIN = 'Net Profit Margin'
+
+# Net Income / Total Assets.
+ROA = 'Return on Assets'
+
+# Net Income / Shareholder Equity.
+ROE = 'Return on Equity'
+
+##########################################################################

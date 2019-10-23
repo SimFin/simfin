cb5893572ce6a09343633b91de2dd370899908e2
Magnus <magnus@hvass-labs.org>
2019-10-23 12:12:05 +0100

    Tiny update

--- simfin/__init__.py
+++ simfin/__init__.py
@@ -9,6 +9,7 @@ from simfin.bulk import load_income, load_income_banks, load_income_insurance
 from simfin.bulk import load_balance, load_balance_banks, load_balance_insurance
 from simfin.bulk import load_cashflow, load_cashflow_banks, load_cashflow_insurance
 
+from simfin.utils import add_date_offset
 from simfin.resample import resample, resample_daily
 
 from simfin.config import set_api_key, load_api_key, get_api_key

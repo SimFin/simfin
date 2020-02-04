b6a789fd10f26551a1a1b74ef0ac7c8ed045c888
Magnus <magnus@hvass-labs.org>
2020-02-04 13:27:41 +0000

    README and setup.py updated project description

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # SimFin - Simple financial data for Python
 
-SimFin makes it easy to obtain and use financial data in Python.
-It automatically downloads data from the [SimFin](https://www.simfin.com/)
-server, saves the data to disk for future use, and loads the data into
-Pandas DataFrames.
+SimFin makes it easy to obtain and use financial and stock-market data in
+Python. It automatically downloads share-prices and fundamental data from
+the [SimFin](https://www.simfin.com/) server, saves the data to disk for
+future use, and loads the data into Pandas DataFrames.
 
 
 ## Example
--- setup.py
+++ setup.py
@@ -10,9 +10,10 @@ setup(
     packages=['simfin'],
     version=MY_VERSION,
     description='Simple financial data for Python',
-    long_description='SimFin makes it easy to obtain and use financial '
-                     'data in Python. It automatically downloads data '
-                     'from the [SimFin](https://www.simfin.com/) server, '
+    long_description='SimFin makes it easy to obtain and use financial and '
+                     'stock-market data in Python. It automatically downloads '
+                     'share-prices and fundamental data from the '
+                     '[SimFin](https://www.simfin.com/) server, '
                      'saves the data to disk for future use, and loads '
                      'the data into Pandas DataFrames.',
     long_description_content_type="text/markdown",
